f34265fb9ba47546961866f0f1c5c7f7735acda5
Vicky <107568580+Vignesh-72@users.noreply.github.com>
2025-04-30 12:26:58 +0530

    Update data_fetcher.py

--- PredictionEngine/data_fetcher.py
+++ PredictionEngine/data_fetcher.py
@@ -1,11 +1,23 @@
+
+from pathlib import Path
+
+import appdirs as ad
+
+CACHE_DIR = ".cache"
+
+# Force appdirs to say that the cache dir is .cache
+ad.user_cache_dir = lambda *args: CACHE_DIR
+
+# Create the cache dir if it doesn't exist
+Path(CACHE_DIR).mkdir(exist_ok=True)
+
+import yfinance as yf
+
 from datetime import datetime, timedelta
 import yfinance as yf
 import pandas as pd
 import time
-import appdirs as ad
 
-# Fix yfinance cache path
-ad.user_cache_dir = lambda *args: "/tmp"
 
 def fetch_stock_data(ticker: str) -> pd.DataFrame:
     """
